f83fb14bccaea7bbdf7a40a9a59c64ab12e0374b
ron <lucifron28@gmail.com>
2025-06-29 19:13:33 +0800

    create profile screen

---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import '../utils/constants.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      appBar: AppBar(
+        title: const Text('Profile'),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: const Center(
+        child: Text(
+          'Profile Screen',
+          style: TextStyle(
+            color: AppColors.textPrimary,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
